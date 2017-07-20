f42a13655f112ffab9e35dcdf45d22c14cd6bfce
kenshirokk <iamkenshiro@live.cn>
2017-07-20 22:20:34 +0800

    init

---
+++ src/main/java/com/kenshiro/bootmvc/BootmvcApplication.java
@@ -0,0 +1,12 @@
+package com.kenshiro.bootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kenshiro/bootmvc/BootmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kenshiro.bootmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
